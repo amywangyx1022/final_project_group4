82e29a603c6813b92bb3ce33e5d2a2ffe07e5c97
Amy_laptop <awang@lakeviewig.com>
2025-03-14 21:07:37 -0500

    figure_1

---
+++ src/calc_functions.py
@@ -0,0 +1,34 @@
+
+import pandas as pd 
+from settings import config
+from pathlib import Path
+DATA_DIR = config("DATA_DIR")
+import pandas as pd
+from pathlib import Path
+
+def calc_pct_returns(data_dir=DATA_DIR):
+    path = Path(data_dir) / "cleaned_bbg_index_data.parquet"
+    df = pd.read_parquet(path)
+    
+    # Define the indices that need yield transformation
+
+    yield_indices = ["USGG30YR Index", "GDBR30 Index", "GJGB30 Index"]
+    
+    # Apply 100 - yield transformation for these indices
+    for idx in yield_indices:
+        if idx in df.columns:
+            df[idx] = 100 / ((1 + df[idx]*0.01) ** 30)
+    pct_change = df.dropna().pct_change()
+    cum_returns = (1 + pct_change).cumprod()  # Ensures it starts at 1
+
+    
+    return cum_returns
+
+
+
+
+if __name__ == "__main__":
+    
+    df = calc_pct_returns(DATA_DIR)
+
+        
